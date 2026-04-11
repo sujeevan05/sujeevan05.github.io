6a8825482e70b479415b6b8bebd813dceb7872b5
sujeevan05 <sujeevan.jeyamohan@dcmail.ca>
2026-04-10 22:48:17 -0400

    First Edits
    Followed the first few edits to create balls and etc.

--- Assignment 4/Part3/main.js
+++ Assignment 4/Part3/main.js
@@ -17,3 +17,55 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+}
+
+class Ball {
+  // …
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+}
+
+const testBall = new Ball(50, 100, 4, 4, "blue", 10);
+
+testBall.x;
+testBall.size;
+testBall.color;
+testBall.draw();
+
+class Ball {
+  // …
+  update() {
+    if (this.x + this.size >= width) {
+      this.velX = -this.velX;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX = -this.velX;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY = -this.velY;
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY = -this.velY;
+    }
+
+    this.x += this.velX;
+    this.y += this.velY;
+  }
+}
