85c63bb41c911cb3d8bb2fe3235181e45fac0541
sujeevan05 <sujeevan.jeyamohan@dcmail.ca>
2026-04-10 22:21:23 -0400

    Last edits
    Added the lighten and darken function.

--- Assignment 4/Part2/main.js
+++ Assignment 4/Part2/main.js
@@ -34,3 +34,14 @@ function updateDisplayedImage(e) {
   displayedImage.src = e.target.src;
   displayedImage.alt = e.target.alt;
 }
+
+btn.addEventListener("click", () => {
+    if (btn.classList.contains("dark")) {
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+    } else {
+      btn.textContent = "Darken";
+      overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+    }
+    btn.classList.toggle("dark");
+  });  
