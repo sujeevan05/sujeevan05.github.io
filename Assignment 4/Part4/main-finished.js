554db0123bf2c8d9c34abb858521b99d66e8f6f0
sujeevan05 <sujeevan.jeyamohan@dcmail.ca>
2026-04-10 23:19:13 -0400

    Chnaged the collisiondetection function

--- Assignment 4/Part4/main-finished.js
+++ Assignment 4/Part4/main-finished.js
@@ -58,7 +58,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
@@ -69,6 +69,7 @@ class Ball {
       }
     }
   }
+  
 }
 
 const balls = [];
