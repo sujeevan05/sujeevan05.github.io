d7a1cbba48c3a85e20c6ca940331126bcd7bf815
sujeevan05 <sujeevan.jeyamohan@dcmail.ca>
2026-04-10 23:21:03 -0400

    Adding the evil circle
    Added the code to create the evil circle.

--- Assignment 4/Part4/main-finished.js
+++ Assignment 4/Part4/main-finished.js
@@ -104,3 +104,20 @@ function loop() {
 }
 
 loop();
+
+window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
