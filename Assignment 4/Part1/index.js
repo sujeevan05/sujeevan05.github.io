680f9a20f18bc0b08284c83e793d4aca04663239
sujeevan05 <sujeevan.jeyamohan@dcmail.ca>
2026-04-10 20:59:31 -0400

    Last part to the file
    Updated the last part of the file.

--- Assignment 4/Part1/index.js
+++ Assignment 4/Part1/index.js
@@ -54,6 +54,6 @@ function generateStory() {
   }
 
   // TODO: replace "" with the correct expression
-  story.textContent = "";
+  story.textContent = newStory;
   story.style.visibility = "visible";
 }
